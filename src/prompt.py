64ebd9d30494bd65497c3c5b6b87542e5dd33d38
damonmo <damonmo@gmail.com>
2024-11-15 15:00:50 +0000

    Updated prompt

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,7 @@
 PROMPT = """
-Generate a valid SQL query for the following natural language instruction:
-
-Query: ${query}
+You are an expert SQL developer. You will be given a prompt which you will have to translate to SQL.
+Please only output the SQL query, no additional information or output markdown.
+Here is the query: ${query}
 
 ${gr.complete_json_suffix_v3}
 """
